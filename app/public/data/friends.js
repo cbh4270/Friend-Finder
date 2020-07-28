2c55f046b6a2f9acef8ad42289f87217781f4b21
cbh4270 <62440425+cbh4270@users.noreply.github.com>
2020-07-27 21:15:50 -0400

    app files

---
+++ app/public/data/friends.js
@@ -0,0 +1,50 @@
+var friends = [
+
+    {
+        name: "Ellie",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGxPxnAIWptWQCudWD6eM1IOsU7RgOJmbb6g&usqp=CAU",
+        scores: [1, 5, 2, 5, 5, 4, 5, 2, 3, 1],
+    },
+    {
+        name: "Curtis",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTTy4QxSe5g2Q51XCM-W92iIbmBAjzNxnE6aQ&usqp=CAU",
+        scores: [4, 5, 4, 2, 3, 4, 3, 5, 3, 4],
+    },
+    {
+        name: "Denzel",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSTa31xKmnI6rYS9nSNXJNOewF7o1E49L6r_Q&usqp=CAU",
+        scores: [5, 4, 3, 1, 5, 5, 2, 1, 1, 5],
+    },
+    {
+        name: "Kim",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwAAgPvjuHXg_-rlVao0J8-v6ZA5gsIFryrg&usqp=CAU",
+        scores: [1, 3, 3, 5, 1, 2, 5, 2, 3, 5],
+    },
+    {
+        name: "Tony",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR1KIhUyK9L6sf5FGLyOUZrwNKq0z_4tK15OQ&usqp=CAU",
+        scores: [4, 1, 4, 5, 3, 5, 1, 5, 3, 5],
+    },
+    {
+        name: "Fiona",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTcf6a7_LWaU-0HW61rN328PX3AbGEkNluINw&usqp=CAU",
+        scores: [2, 5, 1, 5, 2, 2, 1, 2, 3, 4],
+    },
+    {
+        name: "Eugene",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTnrLEfDXkXliz_nNgTtMUYHec16vXYxpGtag&usqp=CAU",
+        scores: [4, 5, 4, 3, 1, 2, 3, 5, 3, 1],
+    },
+    {
+        name: "Violet",
+        photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQpT8uqA65VYuIaocsDYUT5FLNVfaYDYt4pTA&usqp=CAU",
+        scores: [1, 5, 4, 2, 3, 4, 3, 5, 1, 3],
+    },
+    ];
+    
+    module.exports = friends;
+    // https://unsplash.com/photos/TzVN0xQhWaQ
+    // https://unsplash.com/photos/pg_WCHWSdT8
+    // https://unsplash.com/photos/85J99sGggnw
+    
+    // https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQMkGqg5C31GgBsB9aCXe8UIvJ2SkAmx6F-_Q&usqp=CAU
